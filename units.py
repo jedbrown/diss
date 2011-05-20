356e67848f9616849ed8d2e10c0fe1a99de20f64
Jed Brown <jed@59A2.org>
2011-05-20 23:34:39 +0200

    Add nondimensionalization for viscosity

--- units.py
+++ units.py
@@ -28,3 +28,15 @@ grav = 9.81 * meter * second**(-2)
 B = A**(-1/n)
 print('A:',A)
 print('B:',B)
+
+
+def viscosity(Du,p,T):
+    from numpy import exp
+    R = 8.31441 # {\joule\per\mol\per\kelvin}
+    Q = 6.0e4 # {\joule\per\mol} & Activation energy for creep
+    V = -13.e-6 # {\metre\cubed\per\mol} & Activation volume for creep
+    A_0 = 3.61e-13 # {\per\pascal\cubed\per\second} & Softness parameter
+    A = A_0 * exp((-Q + p*V)/(R*T))
+    n = 3
+    gamma = 0.5*Du**2
+    return A**(-1/n) * gamma**((1-n)/(2*n))
